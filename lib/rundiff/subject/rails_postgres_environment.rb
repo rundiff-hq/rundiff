29acaf5e8eb6ba2dd4c14d3a339ef8699da429b7
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-09-18 14:36:27 +0300

    feat: advertise sample-isolated subject state capability

--- lib/rundiff/subject/rails_postgres_environment.rb
+++ lib/rundiff/subject/rails_postgres_environment.rb
@@ -11,6 +11,7 @@ module RunDiff
         telemetry.subject_owned_rails
         runtime.local_process
         state.isolated_comparable
+        state.sample_isolated
         evidence.sql_queries
         evidence.background_jobs
       ].freeze
