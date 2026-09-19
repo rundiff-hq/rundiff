a7cfd149b4b2a456ff1617ab66cdd74e9629c78b
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-09-20 02:46:23 +0300

    Allow disabling YJIT for memory-constrained executor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,4 +7,8 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
+
+  # Keep YJIT available by default, but allow memory-constrained executor
+  # deployments to prefer interpreter memory usage over JIT throughput.
+  config.yjit = false if ENV["RUNDIFF_DISABLE_YJIT"] == "1"
 end
